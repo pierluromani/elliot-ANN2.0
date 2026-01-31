9f2c551c2fc225545bcfbb947f7f5a966ccb72f0
pierluromani <pierluromani@gmail.com>
2026-01-31 12:18:19 +0100

    added simple experiment file item ann lsh

--- sample_fairness_ann_movielens_100k.py
+++ sample_fairness_ann_movielens_100k.py
@@ -2,4 +2,4 @@ from elliot.run import run_experiment
 
 # start from movielens 100k dataset
 print("Done! We are now starting experiment on movielens 1M dataset")
-run_experiment("config_run_experiments/FairANN_configuration_movielens_100k.yml")
+run_experiment("config_run_experiments/item_experiment_movielens_100k.yml")
