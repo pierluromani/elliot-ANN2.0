63df2e860c2a927fc56fa8b70ab6b74d9b2d8397
andreanatile <andrea.natile.an@gmail.com>
2026-02-02 18:00:52 +0100

    andrea files

--- script_run_experiment_movielens_100k.py
+++ script_run_experiment_movielens_100k.py
@@ -1,12 +1,18 @@
-
 from elliot.run import run_experiment
 import warnings
+import builtins
+
+if not hasattr(builtins, 'long'):
+    setattr(builtins, 'long', int)
+
 
 # Suppress all warnings
 warnings.filterwarnings("ignore")
 #
-# print("Done! We are now starting the Fair ANN Elliot's experiment with Item-based methods")
-# run_experiment("config_run_experiments/item_experiment_movielens_100k.yml")
+print(
+    "Done! We are now starting the Fair ANN Elliot's experiment with Item-based methods"
+)
+run_experiment("config_run_experiments/COPIA_item_experiment_movielens_100k.yml")
 
-print("Done! We are now starting the Fair ANN Elliot's experiment with User-based methods")
-run_experiment("config_run_experiments/user_experiment_movielens_100k.yml")
+# print("Done! We are now starting the Fair ANN Elliot's experiment with User-based methods")
+# run_experiment("config_run_experiments/user_experiment_movielens_100k.yml")
