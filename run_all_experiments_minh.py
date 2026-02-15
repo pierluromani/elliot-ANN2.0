614462061b220481547a386f410027646af51620
pierluromani <pierluromani@gmail.com>
2026-02-15 11:54:34 +0100

    added run all script fir minh fixed result path in config files for minh

--- run_all_experiments_minh.py
+++ run_all_experiments_minh.py
@@ -8,7 +8,7 @@ import yaml
 import os
 
 # 0.05
-config_path="config_target_threshold/target_t_0_05.yaml"
+config_path="config_target_threshold/target_t_0_05_minh.yaml"
 excluded_params = ["meta", "path_model"]
 
 create_CR_dataset(config_path, excluded_params)
@@ -16,7 +16,7 @@ create_CR_dataset(config_path, excluded_params)
 run_experiment(config_path)
 
 # 0.25
-config_path="config_target_threshold/target_t_0_25.yaml"
+config_path="config_target_threshold/target_t_0_25_minh.yaml"
 excluded_params = ["meta", "path_model"]
 
 create_CR_dataset(config_path, excluded_params)
@@ -25,7 +25,7 @@ run_experiment(config_path)
 
 # 0.5
 
-config_path="config_target_threshold/target_t_0_50.yaml"
+config_path="config_target_threshold/target_t_0_50_minh.yaml"
 excluded_params = ["meta", "path_model"]
 
 create_CR_dataset(config_path, excluded_params)
@@ -34,7 +34,7 @@ run_experiment(config_path)
 
 #0.75
 
-config_path="config_target_threshold/target_t_0_75.yaml"
+config_path="config_target_threshold/target_t_0_75_minh.yaml"
 excluded_params = ["meta", "path_model"]
 
 create_CR_dataset(config_path, excluded_params)
@@ -42,7 +42,7 @@ create_CR_dataset(config_path, excluded_params)
 run_experiment(config_path)
 
 #0.95
-config_path="config_target_threshold/target_t_0_95.yaml"
+config_path="config_target_threshold/target_t_0_95_minh.yaml"
 excluded_params = ["meta", "path_model"]
 
 create_CR_dataset(config_path, excluded_params)
